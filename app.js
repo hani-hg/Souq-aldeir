0438dc72a45fb59bc155838a81707768e12ad72f
hani-hg <hg787845@gmail.com>
2026-01-13 19:47:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("sw.js")
-    .then(() => {
-      console.log("Service Worker شغّال ✅");
-    })
-    .catch((err) => {
-      console.log("خطأ في Service Worker ❌", err);
-    });
+  navigator.serviceWorker.register("sw.js");
 }
+
+const productsEl = document.getElementById("products");
+const modal = document.getElementById("formModal");
+
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+function render() {
+  productsEl.innerHTML = "";
+  products.forEach(p => {
+    productsEl.innerHTML += `
+      <div class="card">
+        <h4>${p.title}</h4>
+        <p>${p.desc}</p>
+        <strong>${p.price}</strong>
+        <a href="https://wa.me/${p.phone}" target="_blank">واتساب</a>
+      </div>
+    `;
+  });
+}
+
+function openForm() {
+  modal.style.display = "block";
+}
+
+function closeForm() {
+  modal.style.display = "none";
+}
+
+function addProduct() {
+  const p = {
+    title: title.value,
+    price: price.value,
+    phone: phone.value,
+    desc: desc.value
+  };
+  products.unshift(p);
+  localStorage.setItem("products", JSON.stringify(products));
+  closeForm();
+  render();
+}
+
+render();
