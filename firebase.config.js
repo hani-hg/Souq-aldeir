2f9af464934ef9157ede514fa7ba45c962a40ad4
hani-hg <hg787845@gmail.com>
2026-01-14 00:29:52 +0300

    Create firebase.config.js

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBqD...eE4mI",
+  authDomain: "souq-dier.firebaseapp.com",
+  projectId: "souq-dier",
+  storageBucket: "souq-dier.appspot.com",
+  messagingSenderId: "105...203",
+  appId: "1:105...203:web:a0d...f1c"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
