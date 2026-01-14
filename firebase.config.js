6686614f2fcec5f63daa71d524db641e4bf89d74
hani-hg <hg787845@gmail.com>
2026-01-14 12:47:50 +0300

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,13 +1,15 @@
-// تهيئة Firebase لمشروع سوق دير الزور
+// إعدادات Firebase لتطبيق سوق دير الزور
 const firebaseConfig = {
-  apiKey: "AIzaSyA2FhsRLX4SM",
-  authDomain: "souq-aldeir-4.firebaseapp.com",  // أكمل الناقص
-  projectId: "souq-aldeir-4e1b8",  // أكمل الناقص
-  storageBucket: "souq-aldeir-4e1b8.appspot.com",  // أكمل الناقص
-  messagingSenderId: "925621854708",  // أكمل الناقص
-  appId: "1:925621854708:web:1234567890abcdef",  // أكمل الناقص
-  measurementId: "G-J14JVX1HXYZ"  // أكمل الناقص
+  apiKey: "AIzaSyA2FhsRlX4SMpGhzfI0oq_lArSsPTGHUsY",
+  authDomain: "souq-aldeir-4ed7b.firebaseapp.com",
+  projectId: "souq-aldeir-4ed7b",
+  storageBucket: "souq-aldeir-4ed7b.firebasestorage.app",
+  messagingSenderId: "925621854708",
+  appId: "1:925621854708:web:0a830d0684bc75f6be99a1",
+  measurementId: "G-J14JVX1HW4"
 };
 
-// تهيئة Firebase
+// تهيئة Firebase (الطريقة التقليدية للCDN)
 firebase.initializeApp(firebaseConfig);
+
+console.log("✅ تم تهيئة Firebase بنجاح!");
