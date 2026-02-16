4886c203510beed3c8e543908ac4b7296764dce9
hani-hg <hg787845@gmail.com>
2026-02-17 00:19:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js - إعدادات Firebase و Cloudinary
-
+// config.js
 const firebaseConfig = {
   apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
   authDomain: "souq-aldeir-8b708.firebaseapp.com",
@@ -9,13 +8,8 @@ const firebaseConfig = {
   appId: "1:718751448398:web:b588ab6753d1fc7aa1321f"
 };
 
-// تهيئة Firebase (تأكد من تحميل Firebase SDK قبل هذا الملف)
 firebase.initializeApp(firebaseConfig);
-
-// المصادقة وقاعدة البيانات
 const auth = firebase.auth();
 const db = firebase.firestore();
-
-// إعدادات Cloudinary
 const CLOUD_NAME = 'dzjy5tubx';
 const UPLOAD_PRESET = 'souq-aldeir-prseset';
