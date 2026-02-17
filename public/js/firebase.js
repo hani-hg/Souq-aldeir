d1ccf87e8c99fd9d230059b4949d9cbc3f995e21
hani-hg <hg787845@gmail.com>
2026-02-17 11:01:36 +0300

    Create firebase.js

---
+++ public/js/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
+  authDomain: "souq-aldeir-8b708.firebaseapp.com",
+  projectId: "souq-aldeir-8b708"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
