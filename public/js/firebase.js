58ce79def2ab5d77d4990aaf5ed1baf04e36fcc4
hani-hg <hg787845@gmail.com>
2026-02-17 13:41:24 +0300

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,19 +1,8 @@
-// إعدادات Firebase - استبدل هذه القيم بقيم مشروعك
 const firebaseConfig = {
-    apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
-    authDomain: "souq-aldeir-8b708.firebaseapp.com",
-    projectId: "souq-aldeir-8b708",
-    storageBucket: "souq-aldeir-8b708.firebasestorage.app",
-    messagingSenderId: "718751448398",
-    appId: "1:718751448398:web:b588ab6753d1fc7aa1321f"
-    // إذا كنت تستخدم Realtime Database، أضف هذا السطر:
-    // databaseURL: "https://souq-aldeir-8b708-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
+  authDomain: "souq-aldeir-8b708.firebaseapp.com",
+  projectId: "souq-aldeir-8b708"
 };
 
-// تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
-
-// جعل الخدمات متاحة عالمياً
-window.auth = firebase.auth();
-window.db = firebase.firestore();
-window.storage = firebase.storage();
+const db = firebase.firestore();
