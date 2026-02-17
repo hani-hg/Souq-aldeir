ef2d1b84533628375d2dfe9ac09dce4a82cf72f3
hani-hg <hg787845@gmail.com>
2026-02-17 14:00:27 +0300

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,8 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
-  authDomain: "souq-aldeir-8b708.firebaseapp.com",
-  projectId: "souq-aldeir-8b708"
+    apiKey: "AIzaSyCTRpzhyBsf-h9rh2AO6OuIAy_kE7k7fpY",
+    authDomain: "souq-aldeir-8b708.firebaseapp.com",
+    projectId: "souq-aldeir-8b708",
+    storageBucket: "souq-aldeir-8b708.firebasestorage.app",
+    messagingSenderId: "718751448398",
+    appId: "1:718751448398:web:b588ab6753d1fc7aa1321f"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.storage = firebase.storage();
