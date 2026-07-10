3c7fb91a91d48a192660ae7e05de6d628bbdd56d
hani-hg <hg787845@gmail.com>
2026-07-10 20:17:51 +0300

    Update storage.js

--- src/js/Firebase/storage.js
+++ src/js/Firebase/storage.js
@@ -1,1 +1,6 @@
-
+// storage.js
+window.uploadImage = function(file, path = 'ads/') {
+  if (!file) return Promise.resolve(null);
+  const ref = storage.ref(path + Date.now() + '_' + file.name);
+  return ref.put(file).then(snap => snap.ref.getDownloadURL());
+};
