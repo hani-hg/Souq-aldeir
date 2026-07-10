62ccf3e75bb9cbc37d9d29bbc8188dbaecacf974
hani-hg <hg787845@gmail.com>
2026-07-10 23:32:00 +0300

    Delete src/js directory

--- src/js/Firebase/storage.js
+++
@@ -1,6 +0,0 @@
-// storage.js
-window.uploadImage = function(file, path = 'ads/') {
-  if (!file) return Promise.resolve(null);
-  const ref = storage.ref(path + Date.now() + '_' + file.name);
-  return ref.put(file).then(snap => snap.ref.getDownloadURL());
-};
--- src/js/modules/cart.js
+++
@@ -1,8 +0,0 @@
-// cart.js
-window.toggleFav = function(id) {
-  window.showToast('❤️ تمت الإضافة للمفضلة');
-};
-window.openFavs = function() {
-  document.getElementById('favBody').innerHTML = '<p>قائمة المفضلة فارغة حالياً</p>';
-  window.openM('favModal');
-};
