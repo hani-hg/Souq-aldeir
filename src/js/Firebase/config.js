40e4867ca5d70f06b8e235ed270cb56a2b80d885
hani-hg <hg787845@gmail.com>
2026-07-10 20:16:53 +0300

    Update config.js

--- src/js/Firebase/config.js
+++ src/js/Firebase/config.js
@@ -1,1 +1,17 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "// TODO: ضع مفتاح API",
+  authDomain: "// TODO: مشروعك.firebaseapp.com",
+  projectId: "// TODO: مشروعك",
+  storageBucket: "// TODO: مشروعك.appspot.com",
+  messagingSenderId: "// TODO",
+  appId: "// TODO"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
 
+window.db = db;
+window.auth = auth;
+window.storage = storage;
